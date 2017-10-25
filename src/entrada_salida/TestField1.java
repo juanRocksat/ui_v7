e9be9f576a9dd09833122425bd56673c0d4bf4e9
pacoDan1 <dan@10.5.38.246>
2017-10-25 15:11:03 -0300

    Signed-off-by: pacoDan1 <dan@10.5.38.246>

---
+++ src/entrada_salida/TestField1.java
@@ -0,0 +1,54 @@
+package entrada_salida;
+
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JTextField;
+
+import graficos_v1.MostrarImagen;
+
+public class TestField1 extends JFrame implements ActionListener
+{
+	public JTextField textField1=new JTextField();
+	public JLabel label1=new JLabel("Usuario: ");
+	public JButton boton1=new JButton("Aceptar->");
+	
+	public void setear()
+	{
+		setLayout(null);
+		label1.setBounds(10,10,100,30);
+		textField1.setBounds(120,20,150,20);
+		boton1.setBounds(10,80,100,30);
+		add(boton1);add(textField1);add(label1);
+		boton1.addActionListener(this);
+	}
+	@Override
+	public void actionPerformed(ActionEvent e)
+	{
+		if(e.getSource()==boton1)
+		{
+			setTitle(textField1.getText());
+		}	
+	}
+	private void mostrar()
+	{
+		setear();
+		setBounds(0,0,300,150);
+		setVisible(true);
+	}
+	private static  void disponiblePorSegundos(long  segundos) throws InterruptedException
+	{
+		Thread.sleep(segundos*1000);
+		System.exit(ABORT);
+		System.exit(0);
+	}
+	public static void main(String[] args) throws InterruptedException
+	{
+		System.out.println("puts");
+		(new TestField1()).mostrar();
+		disponiblePorSegundos(8);
+	}
+}
